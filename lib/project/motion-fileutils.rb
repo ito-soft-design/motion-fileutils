dedbd5629415fc0990d1d456907a1cfedeeee9b0
Katsuyoshi Ito <kito@itosoft.com>
2013-06-09 11:51:32 +0900

    add mkdir and rm

--- lib/project/motion-fileutils.rb
+++ lib/project/motion-fileutils.rb
@@ -1,6 +1,64 @@
-class Motion
-  class Fileutils
-
+module Motion
+  module FileUtils
+    
+    def self.private_module_function(name)   #:nodoc:
+      module_function name
+      private_class_method name
+    end
+  
+    def mkdir(dir, options = {})
+      mkdir_with_intermediate(dir, false, options)
+    end
+    module_function :mkdir
+  
+    def mkdir_p(dir, options = {})
+      mkdir_with_intermediate(dir, true, options)
+    end
+    module_function :mkdir_p
+  
+    alias :mkpath :mkdir_p
+    module_function :mkpath
+  
+    alias :makedirs :mkdir_p
+    module_function :makedirs
+  
+    def rm(list, options = {})
+      error = Pointer.new(:object)
+      m = NSFileManager.defaultManager
+      list = [list] unless list.is_a? Array
+      list.each do |path|
+        r = m.removeItemAtPath path, error:error
+#p error, r unless r
+      end
+    end
+    module_function :rm
+  
+    alias :remove :rm
+    module_function :remove
+  
+    private
+  
+    def mkdir_with_intermediate(dir, intermediate, options = {})
+      error = Pointer.new(:object)
+      m = NSFileManager.defaultManager
+      dir = [dir] unless dir.is_a? Array
+      paths = []
+      dir.each do |path|
+        r = m.createDirectoryAtPath path, withIntermediateDirectories:intermediate, attributes:nil, error:error
+        paths << path if r
+#p error, r unless r
+      end
+      case paths.size
+      when 0
+        nil
+      when 1
+        paths.first
+      else
+        paths
+      end
+    end
+    private_module_function :mkdir_with_intermediate
+  
+  
   end
 end
-
---
+++ spec/motion-fileutils_spec.rb
@@ -0,0 +1,83 @@
+describe "Motion::FileUtils" do
+  before do
+    @path = "abc".cache
+    @sub_path = "abc/def".cache
+  end
+  
+  after do
+    Motion::FileUtils.rm @path
+    Motion::FileUtils.rm @sub_path
+  end
+
+  describe "mkdir" do
+  
+    it "should create a directory" do
+      Motion::FileUtils.mkdir @path
+      @path.exists?.should == true
+    end
+    
+    it "should not create a directory" do
+      Motion::FileUtils.mkdir @sub_path
+      @sub_path.exists?.should == false
+    end
+    
+  end
+  
+  describe "mkdir_p" do
+  
+    it "should create a directory" do
+      r = Motion::FileUtils.mkdir_p @path
+      r.should == @path
+      @path.exists?.should == true
+    end
+    
+    it "should create a directory" do
+      r = Motion::FileUtils.mkdir_p @sub_path
+      r.should == @sub_path
+      @sub_path.exists?.should == true
+    end
+    
+    it "should create a directory" do
+      r = Motion::FileUtils.mkdir_p [@path, @sub_path]
+      r.should == [@path, @sub_path]
+      @path.exists?.should == true
+      @sub_path.exists?.should == true
+    end
+    
+    it "should create a directory using mkpath" do
+      Motion::FileUtils.mkpath @path
+      @path.exists?.should == true
+    end
+    
+    it "should create a directory using makedirs" do
+      Motion::FileUtils.makedirs @path
+      @path.exists?.should == true
+    end
+    
+  end
+  
+  it "should remove a directory" do
+    Motion::FileUtils.mkdir @path
+    Motion::FileUtils.rm @path
+    @path.exists?.should == false
+  end
+  
+  it "should remove a directory" do
+    Motion::FileUtils.mkdir @path
+    Motion::FileUtils.remove @path
+    @path.exists?.should == false
+  end
+  
+end
+
+
+
+class NSString
+  def cache
+    NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, true)[0].stringByAppendingPathComponent(self)
+  end
+  def exists?
+    NSFileManager.defaultManager.fileExistsAtPath(self)
+  end
+
+end
