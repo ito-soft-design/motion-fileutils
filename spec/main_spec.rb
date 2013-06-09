7ebd3f58e986e1168ca7650f41d5d9f25f72aa8c
Katsuyoshi Ito <kito@itosoft.com>
2013-06-09 11:50:49 +0900

    remove main_spec.rb

--- spec/main_spec.rb
+++
@@ -1,9 +0,0 @@
-describe "Application 'motion-fileutils'" do
-  before do
-    @app = UIApplication.sharedApplication
-  end
-
-  it "has one window" do
-    @app.windows.size.should == 1
-  end
-end
